b125d498dab6022e9492ef656e6ebe5ab25de11c
tr0v40 <34144357+tr0v40@users.noreply.github.com>
2018-04-14 18:05:27 -0300

    nova

--- docs/core/views.py
+++ docs/core/views.py
@@ -3,35 +3,92 @@ from core import views
 
 # Create your views here.
 def index(request):
-    context ={
+    context = {
         "titulo": "Home",
 
     }
     return render(request, 'index.html', context)
 
+
 def cursos(request):
+    context = {
+        "titulo": "Cursos",
+
+    }
     return render(request, 'cursos.html')
 
 def detalhes(request):
+    context = {
+        "titulo": "Detalhes",
+
+    }
     return render(request, 'detalhes-da-disciplina.html')
 
 def detalhes_curso(request):
+    context = {
+        "titulo": "Detalhe do Curso",
+
+    }
     return render(request, 'detalhes-do-curso.html')
 
 def formcur(request):
+    context = {
+        "titulo": "Curso",
+
+    }
     return render(request, 'formulario-curso.html')
 
 def formdisc(request):
+    context = {
+        "titulo": "Disciplina",
+
+    }
     return render(request, 'formulario_disciplina.html')
 
 def login(request):
+    context = {
+        "titulo": "Login",
+
+    }
     return render(request, 'login.html')
 
 def matricula(request):
+    context = {
+        "titulo": "Matricula",
+
+    }
     return render(request, 'matricula.html')
 
 def novo_aluno(request):
+    context = {
+        "titulo": "Cadastro de Aluno",
+
+    }
     return render(request, 'novo_aluno.html')
 
 def pag_disc(request):
+    context = {
+        "titulo": "Disciplina",
+
+    }
     return render(request, 'pagina_da_disciplina.html')
+----------
+def bd(request):
+    context = {
+        "titulo": "Disciplina",
+        "paragafo1": ["
+
+        "
+         ]
+    }
+    return render(request, 'pagina_da_disciplina.html')
+
+def ads(request):
+    context = {
+        "titulo": "Disciplina",
+        "paragafo1": ["
+        Minha caceta
+        "
+         ]
+    }
+    return render(request, 'ads.html')
--- docs/lms/urls.py
+++ docs/lms/urls.py
@@ -19,16 +19,18 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
-    path('cursos/', views.cursos),
-    path('detalhes_da_disciplina/', views.detalhes),
-    path('detalhes-do-curso/', views.detalhes_curso),
-    path('formulario-curso/', views.formcur),
-    path('formulario_discipĺina/', views.formdisc),
-    path('login/', views.login),
-    path('matricula/', views.matricula),
-    path('novo_aluno/', views.novo_aluno),
-    path('pagina_da_disciplina/', views.pag_disc),
+    path('', views.index, name=" "),
+    path('cursos/', views.cursos, name="cursos"),
+    path('detalhes_da_disciplina/', views.detalhes, name="detalhes_da_disciplina"),
+    path('detalhes-do-curso/', views.detalhes_curso, name="detalhes-do-curso"),
+    path('formulario-curso/', views.formcur, name="formulario-curso"),
+    path('formulario_discipĺina/', views.formdisc, name="formulario_discipĺina"),
+    path('login/', views.login, name="login"),
+    path('matricula/', views.matricula, name="matricula"),
+    path('pagina_da_disciplina/', views.pag_disc, name="pagina_da_disciplina"),
+    path('novo_aluno/', views.novo_aluno, name="novo_aluno"),
+    path('ads/', views.ads, name="ads"),
+
 
 
 ]
